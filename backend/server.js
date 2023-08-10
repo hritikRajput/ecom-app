d6b0dbb3c504a045288da5a315ff7c6ee4fe4620
Ritik Rajput <hritikshivam@gmail.com>
2023-08-10 13:02:47 +0530

    Add route for /api/products in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const products = require("./db/products");
 
 //creating a server
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello there!");
+});
+
+app.get("/api/products", (req, res) => {
+  res.status(200).json(products);
 });
 
 app.listen(3000, () => {
