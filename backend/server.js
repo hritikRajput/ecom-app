8403edca234d2e7f06514afc14f0017bd1aac675
Ritik Rajput <hritikshivam@gmail.com>
2023-08-10 16:05:45 +0530

    Add route for /api/auth/login in server.js for post method

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const products = require("./db/products");
+const users = require("./db/users");
 
 //creating a server
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello there!");
@@ -10,6 +12,20 @@ app.get("/", (req, res) => {
 
 app.get("/api/products", (req, res) => {
   res.status(200).json(products);
+});
+
+app.get("/api/auth/login", (req, res) => {
+  res.status(200).json(users);
+});
+
+app.post("/api/auth/login", (req, res) => {
+  const isUserRegistered = users.data.some(
+    (user) =>
+      user.number === req.body.number && user.password === req.body.password
+  );
+  isUserRegistered
+    ? res.status(200).json({ message: "User found" })
+    : res.status(401).json({ message: "User not found" });
 });
 
 app.listen(3000, () => {
