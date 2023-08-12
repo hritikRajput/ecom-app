98d30f2f0341caa48c6a276c067e033f21691e97
Ritik Rajput <hritikshivam@gmail.com>
2023-08-12 22:02:54 +0530

    Add User model

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,19 +1,24 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
-const productSchema = new Schema({
-  name: String,
-  author: String,
-  coverImage: String,
-  description: String,
-  price: Number,
-  format: String,
-  genre: Array,
-  pages: Number,
-  publisher: String,
-  language: String,
-  rating: Number,
-  isInStock: Boolean,
-});
+const productSchema = new Schema(
+  {
+    name: String,
+    author: String,
+    coverImage: String,
+    description: String,
+    price: Number,
+    format: String,
+    genre: Array,
+    pages: Number,
+    publisher: String,
+    language: String,
+    rating: Number,
+    isInStock: Boolean,
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Product = mongoose.model("products", productSchema);
 
--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,6 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const db = require("./config/db");
 
-const users = require("./db/users");
-const Product = require("./models/Product");
-
 const productRouter = require("./routes/productRoutes");
 
 //creating a server
@@ -20,20 +17,6 @@ app.get("/", (req, res) => {
 
 //route for "/api/products"
 app.use("/api/products", productRouter);
-
-// POST route to add multiple products
-app.use("/api/products", productRouter);
-
-//route for "/api/auth/login"
-app.post("/api/auth/login", (req, res) => {
-  const isUserRegistered = users.data.some(
-    (user) =>
-      user.number === req.body.number && user.password === req.body.password
-  );
-  isUserRegistered
-    ? res.status(200).json({ message: "User found" })
-    : res.status(401).json({ message: "User not found" });
-});
 
 mongoose.connection.once("open", () => {
   console.log("connected to db");
