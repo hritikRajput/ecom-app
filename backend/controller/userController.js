760b163ed378b011466769b9c644ace708ad447d
Ritik Rajput <hritikshivam@gmail.com>
2023-08-12 23:05:46 +0530

    Add loginUser for /api/auth/login route

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -19,6 +19,7 @@ const createUser = async (req, res) => {
         .status(400)
         .json({ error: "User with this number already exists." });
     }
+
     const createdUser = await User.create(newUser);
     res.status(201).json(createdUser);
   } catch (err) {
@@ -30,4 +31,25 @@ const createUser = async (req, res) => {
   }
 };
 
-module.exports = { createUser };
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ error: "User not found." });
+    }
+    const isPasswordCorrect = await User.findOne({ email, password });
+    if (!isPasswordCorrect) {
+      return res.status(401).json({ error: "Invalid credentials." });
+    }
+    res.status(200).json({ message: "Login successful.", user });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      error: "An error occurred while logging in",
+      err,
+    });
+  }
+};
+
+module.exports = { createUser, loginUser };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { createUser } = require("../controller/userController");
+const { createUser, loginUser } = require("../controller/userController");
 
 router.route("/signup").post(createUser);
+router.route("/login").post(loginUser);
 
 module.exports = router;
