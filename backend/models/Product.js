eacf51cd131cd060c26d2a5e2b35875a422218b4
Ritik Rajput <hritikshivam@gmail.com>
2023-08-18 00:31:22 +0530

    Update Product schema

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -2,19 +2,25 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 const productSchema = new Schema(
   {
-    name: String,
+    title: String,
     author: String,
+    isbn: String,
     coverImage: String,
     description: String,
     price: Number,
+    discount: Number,
     format: String,
     genre: Array,
     pages: Number,
+    publishedDate: String,
     publisher: String,
     language: String,
     rating: Number,
+    reviews: Array,
+    dimensions: Object,
     isInStock: Boolean,
   },
+
   {
     timestamps: true,
   }
