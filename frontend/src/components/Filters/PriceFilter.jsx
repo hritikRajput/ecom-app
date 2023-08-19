537b383551522ab1fa29b14a3b2f5df4e45e8b91
Ritik Rajput <hritikshivam@gmail.com>
2023-08-20 01:10:56 +0530

    Update PriceFilter

--- frontend/src/components/Filters/PriceFilter.jsx
+++ frontend/src/components/Filters/PriceFilter.jsx
@@ -3,11 +3,11 @@ const PriceFilter = () => {
         <div className="mb-2">
             <div className="bg-gray-lighter px-4 py-4 mb-1">PRICE</div>
             <ul className="filters-ul">
-                <li><span className="cursor-pointer">Under &#8377;250</span></li>
-                <li><span className="cursor-pointer">&#8377;250-500</span></li>
+                <li><span className="cursor-pointer">Under &#8377;500</span></li>
                 <li><span className="cursor-pointer">&#8377;500-1000</span></li>
                 <li><span className="cursor-pointer">&#8377;1000-1500</span></li>
-                <li><span className="cursor-pointer">Over &#8377;1500</span></li>
+                <li><span className="cursor-pointer">&#8377;1500-2000</span></li>
+                <li><span className="cursor-pointer">Over &#8377;2000</span></li>
             </ul>
         </div>
     )
