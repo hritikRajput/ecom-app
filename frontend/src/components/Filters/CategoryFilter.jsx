44cf6de06313dbb1f2aa2e08dd66c9e2042b489e
Ritik Rajput <hritikshivam@gmail.com>
2023-08-20 01:06:01 +0530

    Fix spelling mistake

--- frontend/src/components/Filters/CategoryFilter.jsx
+++ frontend/src/components/Filters/CategoryFilter.jsx
@@ -4,7 +4,7 @@ const CategoryFilter = () => {
             <div className="bg-gray-lighter px-4 py-4 mb-1">CATEGORY</div>
             <ul className="filters-ul">
                 <li><span className="cursor-pointer">Science</span></li>
-                <li><span className="cursor-pointer">Myster</span></li>
+                <li><span className="cursor-pointer">Mystery</span></li>
                 <li><span className="cursor-pointer">Romance</span></li>
                 <li><span className="cursor-pointer">History</span></li>
                 <li><span className="cursor-pointer">Self Help</span></li>
