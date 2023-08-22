fb26b48e258750ed3ef4e16420e982a8dea25566
Ritik Rajput <hritikshivam@gmail.com>
2023-08-23 05:24:37 +0530

    Update Wishlist model

--- backend/models/WishList.js
+++ backend/models/WishList.js
@@ -2,19 +2,26 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 const wishListSchema = new Schema(
   {
-    name: String,
+    title: String,
     author: String,
+    isbn: String,
     coverImage: String,
     description: String,
     price: Number,
+    discount: Number,
+    discountedPrice: Number,
     format: String,
     genre: Array,
     pages: Number,
+    publishedDate: String,
     publisher: String,
     language: String,
     rating: Number,
+    reviews: Array,
+    dimensions: Object,
     isInStock: Boolean,
   },
+
   {
     timestamps: true,
   }
