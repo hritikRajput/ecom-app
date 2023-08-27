0575a2c76be26061aea8e0ed74b3f3c516b31f68
Ritik Rajput <hritikshivam@gmail.com>
2023-08-27 22:10:54 +0530

    Fix error in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Products from "./pages/Products/Products";
 import Home from "./pages/Home/Home";
-import WishList from "./pages/Wishlist/WishList";
+import WishList from "./pages/WishList/WishList";
 import Signup from "./pages/Auth/Signup";
 import Login from "./pages/Auth/Login";
 import Cart from "./pages/Cart/Cart";
