f6c3d50c5ea8dcd2fadbe682619264eaf77a2928
Ritik Rajput <hritikshivam@gmail.com>
2023-09-01 13:41:47 +0530

    Fix issue with hero image

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,19 +1,14 @@
 import Footer from "../../components/Footer/Footer"
 import Header from "../../components/Header/Header"
-import useWishList from "../../context/useWishList"
-import bookstack from "../../assets/bookstack1.jpg"
 
 const Home = () => {
-    const { state } = useWishList()
-    console.log(state)
-
     return (
         <>
             <Header />
             <main>
                 <div className="flex items-center gap-8 py-4 min-w-[320px] w-4/5 mx-auto">
                     <h2 className="w-1/2 font-bold text-5xl text-color-dark-text">Discover new worlds with every page</h2>
-                    <div className="h-[420px] w-full object-cover bg-[url('src/assets/bookstack1.jpg')] rounded-md">
+                    <div className="bg-hero-books h-[420px] w-full object-cover rounded-md">
                     </div>
                 </div>
             </main>
