3d161fe7ea1a1372e75f4b15671f4391f9dadd6f
Ritik Rajput <hritikshivam@gmail.com>
2023-09-01 13:57:32 +0530

    Fix issue with hero image repeat

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
             <main>
                 <div className="flex items-center gap-8 py-4 min-w-[320px] w-4/5 mx-auto">
                     <h2 className="w-1/2 font-bold text-5xl text-color-dark-text">Discover new worlds with every page</h2>
-                    <div className="bg-hero-books h-[420px] w-full object-cover rounded-md">
+                    <div className="bg-hero-books bg-no-repeat h-[420px] w-full object-cover rounded-md">
                     </div>
                 </div>
             </main>
